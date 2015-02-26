bd163bc0275bc34edddef81738130abc46df898a
Phill Conrad <pconrad@cs.ucsb.edu>
2015-02-26 13:34:04 -0800

    Latest, though not working

--- src/edu/ucsb/cs56/projects/github/jcapi_demo/Demo1.java
+++ src/edu/ucsb/cs56/projects/github/jcapi_demo/Demo1.java
@@ -1,14 +1,38 @@
+package edu.ucsb.cs56.projects.github.jcapi_demo;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import com.jcabi.github.RtGithub;
+import com.jcabi.github.Issues;
 import com.jcabi.github.Issue;
 import com.jcabi.github.Github;
+import com.jcabi.github.Repos;
 import com.jcabi.github.Repo;
+import com.jcabi.github.User;
+import com.jcabi.github.Coordinates;
 
 public class Demo1 {
 
-    public static void main(String[] args) {
-	Github github = new RtGithub(".. your OAuth token ..");
-	Repo repo = github.repos().get(new Coordinates.Simple("jcabi", "jcabi-github"));
-	Issue issue = repo.issues().create("How are you?", "Please tell me...");
-	issue.comments().post("My first comment!");
+    public static String readAllBytes(String filename) throws Exception {
+      return new String(Files.readAllBytes(Paths.get(filename)));
+    }
+    
+    public static void main(String[] args) throws Exception {
+	String oauthToken = Demo1.readAllBytes("tokens/MostPrivileges.txt");
+	Github github = new RtGithub(oauthToken);
+	System.out.println("Github=" + github);
+	Repos repos = github.repos();
+	System.out.println("repos=" + repos);
+	Repo repo = repos.get(new Coordinates.Simple("jcabi/jcabi-github"));
+	Issues issues = repo.issues();
+	Issue issue = issues.get(123);
+	User author = new Issue.Smart(issue).author();
+	System.out.println("author="+author);
+
     }
 }
+
+
